ed5586fb2991e9fa127d73dc00281b5a22d78301
daniel <danielk0331@naver.com>
2023-02-07 02:12:24 +0900

    'a'

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CatsController } from './cats/cats.controller';
 import { CatsModule } from './cats/cats.module';
 import { LoggerMiddleware } from './common/middleware/logger.middleware';
 
